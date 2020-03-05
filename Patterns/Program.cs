0f209f4153e00f337a6e41644bbf95821088d0d6
Alexey Tolkov <TolkovAS@GMail.com>
2020-03-05 11:38:09 +0500

    Add project files.

---
+++ Patterns/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Patterns
+{
+    public enum Size
+    {
+        Small, Medium, Large, Huge
+    } 
+
+    public class Product
+    {
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var s = Size.Medium;
+        }
+    }
+}
